710b32bf7bdffbedd1c5cc66e205420b171d396d
Kamil Zalwert <kamilz89@gmail.com>
2020-06-02 13:04:41 +0200

    16.2 - Menu użytkownika

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> cardsPL = CardDeckUtils.getListOfCards(CardDeckUtils.POLISH);
-        List<String> cardsEN = CardDeckUtils.getListOfCards(CardDeckUtils.ENGLISH);
-
-        System.out.println("--- Talia 52 kart po polsku ---");
-        for (int i = 1; i <= cardsPL.size(); i++) {
-            System.out.println(i + ".\t" + cardsPL.get((i - 1)));
-        }
-
-        System.out.println("--- Talia 52 kart po angielsku ---");
-        for (int i = 1; i <= cardsEN.size(); i++) {
-            System.out.println(i + ".\t" + cardsEN.get((i - 1)));
-        }
-
-        System.out.println("--- koniec ---");
+        CardDeckUtils.runMenu();
     }
 }
--- src/logic/CardDeckUtils.java
+++ src/logic/CardDeckUtils.java
@@ -5,6 +5,7 @@ import data.CardDeck;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CardDeckUtils {
 
@@ -13,6 +14,43 @@ public class CardDeckUtils {
 
     public static final String POLISH = "PL";
     public static final String ENGLISH = "EN";
+    public static final String QUIT = "Q";
+
+    public static void runMenu(){
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        do{
+            System.out.println("-------------------------------------\n" +
+                    "Podaj kod języka/Choose language code\n" +
+                    "PL - Polish/Polski\n" +
+                    "EN - English/Angielski\n" +
+                    "Q  - Quit/Koniec");
+            String countryCode = scanner.nextLine();
+            switch(countryCode.toUpperCase()) {
+                case POLISH -> {
+                    List<String> cardsPL = CardDeckUtils.getListOfCards(CardDeckUtils.POLISH);
+                    System.out.println("--- Talia 52 kart po polsku ---");
+                    for (int i = 1; i <= cardsPL.size(); i++) {
+                        System.out.println(i + ".\t" + cardsPL.get((i - 1)));
+                    }
+                }
+                case ENGLISH -> {
+                    List<String> cardsEN = CardDeckUtils.getListOfCards(CardDeckUtils.ENGLISH);
+                    System.out.println("--- 52 Cards in English ---");
+                    for (int i = 1; i <= cardsEN.size(); i++) {
+                        System.out.println(i + ".\t" + cardsEN.get((i - 1)));
+                    }
+                }
+                case QUIT -> {
+                    System.out.println("--- koniec ---");
+                    loop=false;
+                }
+                default -> {
+                    System.out.println("Wybrano zły kod: " + countryCode);
+                }
+            }
+        } while (loop);
+    }
 
     public static List<String> getListOfCards(String language) {
         List<String> listOfCards = new ArrayList<>();
